4796dde9d5a8b4bbd8ccacfefab8cbda2f03f56e
Umar8213 <umar.draz@techbolic.com>
2025-02-01 17:58:41 +0500

    make setup of form which include store dropdown_input and products checkbox_input to transfer products of current login store to the store currently displayed in dropdown_input, add route of post request, and remove csrf authentication for making things work.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,19 @@ class HomeController < ApplicationController
   include ShopifyApp::EmbeddedApp
   include ShopifyApp::EnsureInstalled
   include ShopifyApp::ShopAccessScopesVerification
+  skip_before_action :verify_authenticity_token, only: [:transfer_products]
 
   def index
     if ShopifyAPI::Context.embedded? && (!params[:embedded].present? || params[:embedded] != "1")
       redirect_to(ShopifyAPI::Auth.embedded_app_url(params[:host]) + request.path, allow_other_host: true)
     else
+      @shops = Shop.where.not(shopify_domain: current_shopify_domain)
       @shop_origin = current_shopify_domain
       @host = params[:host]
     end
+  end
+
+  def transfer_products
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/products', :to => 'products#index'
   mount ShopifyApp::Engine, at: '/'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post '/transfer_products', to: 'home#transfer_products', as: :transfer_products 
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
